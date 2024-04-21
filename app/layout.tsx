1bc86b51195d62e3f9e2c1ba7b06c4d9453b2efe
bjklee <bjklee@umich.edu>
2024-04-20 23:33:00 -0400

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(font.className, "bg-white dark:bg-[rgb(29,33,42)]")}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          storageKey="portfolio"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
-
+import { ModeToggle } from "@/components/mode-toggle";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+} from "@/components/ui/dropdown-menu";
+import { Menu } from "lucide-react";
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-bold">portfolio</p>
+      <div className="absolute left-0 top-0">
+        <p className="text-2xl m-5">Brendan Lee</p>
+      </div>
+
+      <div className="absolute top-0 right-0 m-5 space-x-3">
+        <ModeToggle />
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Menu className="absolute h-[1.2rem] w-[1.2rem]" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>About Me</DropdownMenuLabel>
+            <DropdownMenuItem>Experience</DropdownMenuItem>
+            <DropdownMenuItem>Skills</DropdownMenuItem>
+            <DropdownMenuItem>Projects</DropdownMenuItem>
+            <DropdownMenuItem>Contact Me</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 }
