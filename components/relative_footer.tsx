4dec3500e0c41dfa7649d90d9f8a3b3472dd1184
bjklee <bjklee@umich.edu>
2024-04-22 02:34:03 -0400

    add email to footer

--- components/relative_footer.tsx
+++ components/relative_footer.tsx
@@ -9,10 +9,15 @@ export default function RelativeFooter() {
       <div className="flex flex-row justify-between">
         <p className="text-sm">© Brendan Lee 2024</p>
         <div className="flex flex-row">
-          <a href="https://github.com/bjklee" className="mr-4">
+          <a href="mailto:bjklee@umich.edu" className="mx-1 text-sm">
+            bjklee@umich.edu
+          </a>
+          <p className="text-sm">|</p>
+          <a href="https://github.com/bjklee" className="mx-1">
             <Github strokeWidth={1} />
           </a>
-          <a href="https://www.linkedin.com/in/brendanjklee/">
+          <p className="text-sm">|</p>
+          <a href="https://www.linkedin.com/in/brendanjklee/" className="ml-1">
             <Linkedin strokeWidth={1} />
           </a>
         </div>
